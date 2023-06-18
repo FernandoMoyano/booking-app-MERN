d5c711ed324e6cb5fdfc0d80b10ac33c13c80e2b
FernandoMoyano <fernandomoyano21@gmail.com>
2023-06-17 22:04:48 -0300

    LoginPage-component

--- client/src/pages/LoginPage/LoginPage.jsx
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <div className="mt-4 grow flex items-center justify-around">
+      <div className="mb-64">
+        <h1 className="text-4xl text-center mb-4">Login</h1>
+        <form className="max-w-md mx-auto">
+          <input type="email" placeholder="Your Email"></input>
+          <input type="password" placeholder="password"></input>
+          <button className="primary">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
