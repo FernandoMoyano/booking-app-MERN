2455dcedf4a03898b33cf79ec827bd9eef7eb4bb
FernandoMoyano <fernandomoyano21@gmail.com>
2023-06-20 18:34:27 -0300

    form state-intall axios-cors

--- client/src/pages/RegisterPage/RegisterPage.jsx
+++ client/src/pages/RegisterPage/RegisterPage.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function registerUser(event) {
+    event.preventDefault();
+  }
+
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
         <h1 className="text-4xl text-center mb-4">Register</h1>
-        <form className="max-w-md mx-auto">
-          <input type="text" placeholder="Fernando Moyano"></input>
-          <input type="email" placeholder="Your Email"></input>
-          <input type="password" placeholder="password"></input>
-          <button className="primary">Register now</button>
+        <form className="max-w-md mx-auto" onSubmit={registerUser}>
+          <input
+            type="text"
+            placeholder="Fernando Moyano"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          ></input>
+          <input
+            type="email"
+            placeholder="Your Email"
+            value={email}
+            onChange={(event) => setEmail(event.target.email)}
+          ></input>
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          ></input>
+          <button className="primary">Register</button>
           <div className="text-center py-2 text-gray-500">
             Already a member?
             <Link className="underline text-black ml-1" to={"/login"}>
