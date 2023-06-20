6efc8357e0b3b009632c5a8b6936794687873543
FernandoMoyano <fernandomoyano21@gmail.com>
2023-06-20 18:01:53 -0300

    RegisterPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import IndexPage from "./pages/IndexPage/IndexPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import Layout from "./Layout/Layout";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />}></Route>
         <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/register" element={<RegisterPage />}></Route>
       </Route>
     </Routes>
   );
--- client/src/pages/RegisterPage/RegisterPage.jsx
+++ client/src/pages/RegisterPage/RegisterPage.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const LoginPage = () => {
+const RegisterPage = () => {
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
-        <h1 className="text-4xl text-center mb-4">Login</h1>
+        <h1 className="text-4xl text-center mb-4">Register</h1>
         <form className="max-w-md mx-auto">
+          <input type="text" placeholder="Fernando Moyano"></input>
           <input type="email" placeholder="Your Email"></input>
           <input type="password" placeholder="password"></input>
-          <button className="primary">Login</button>
+          <button className="primary">Register now</button>
           <div className="text-center py-2 text-gray-500">
-            Don't have an account yet?
-            <Link className="underline text-black ml-1" to={"/register"}>Register now</Link>
+            Already a member?
+            <Link className="underline text-black ml-1" to={"/login"}>
+              Login
+            </Link>
           </div>
         </form>
       </div>
@@ -20,4 +23,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default RegisterPage;
