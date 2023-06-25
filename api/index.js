c520952b27770266243d78be769a521718cabfda
FernandoMoyano <fernandomoyano21@gmail.com>
2023-06-25 12:26:53 -0300

    UserModel-isntall-dotenv-bcrypt

--- api/index.js
+++ api/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+const bcrypt = require("bcrypt");
+const User = require("./models/User");
+require("dotenv").config();
+
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 app.use(
   cors({
     credentials: true,
@@ -10,14 +15,20 @@ app.use(
   })
 );
 
+//database connection
+mongoose.connect(process.env.MONGO_URL);
+
 app.get("/test", (req, res) => {
   res.json("test Ok");
 });
 
 app.post("/register", (req, res) => {
   const { name, email, password } = req.body;
-  res.json({name,email,password})
+  User.create({
+    name,
+    email,
+    password: bcrypt.hashSync(password, secret),
+  });
 });
 
 app.listen(4000);
-//xZTZkj9IMiIeZ51K
